672e5657d411d9a8687b4102882759bb427fd882
Ravi Kalia <ravkalia@gmail.com>
2020-08-20 21:44:14 -0400

    style(components): add outline of components

---
+++ games/memory_game/app.js
@@ -0,0 +1,21 @@
+import cardArray from "./cardArray";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  const score = document.querySelector("#score");
+
+  const cardChosen = [];
+  const cardChosenId = [];
+  const cardsWon = [];
+
+  const createBoard = () => {
+    // create cells, add emoji and then hide emoji
+    // attach event handler that looks up specific card
+  };
+
+  const isMatch = () => {};
+
+  const revealCard = () => {};
+  // show card for a timeOut, check if match, and then show as completed if match
+  //
+});
---
+++ games/memory_game/cardArray.js
@@ -0,0 +1,42 @@
+export default const cardArray = [
+  {
+    name: "discover",
+    emoji: "󠀠󠀠❓",
+  },
+  {
+    name: "Canada",
+    emoji: "🇨🇦",
+  },
+  {
+    name: "Mexico",
+    emoji: "🇲🇽",
+  },
+  {
+    name: "United States",
+    eomji: "🇺🇸",
+  },
+  {
+    name: "China",
+    emoji: "🇨🇳",
+  },
+  {
+    name: "Germany",
+    emoji: "🇩🇪",
+  },
+  {
+    name: "Japan",
+    emoji: "🇯🇵",
+  },
+  {
+    name: "France",
+    emoji: "🇫🇷",
+  },
+  {
+    name: "Italy",
+    emoji: "🇮🇹",
+  },
+  {
+    name: "Spain",
+    emoji: "🇪🇸",
+  },
+];
